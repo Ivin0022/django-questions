0cd6451de923dfeb1556f030bbbb170442b60218
ivin stephen <ivinstephen001@gmail.com>
2020-05-25 03:06:25 +0530

    added choice answer

--- questions/admin/__init__.py
+++ questions/admin/__init__.py
@@ -2,3 +2,4 @@ from .text_question import TextQuestionAdmin
 from .text_answer import TextAnswer
 
 from .choice_question import ChoiceQuestionAdmin
+from .choice_answer import ChoiceAnswerAdmin
--- questions/models/__init__.py
+++ questions/models/__init__.py
@@ -3,3 +3,4 @@ from .text_answer import TextAnswer
 
 from .choice_question import ChoiceQuestion
 from .choices import Choice
+from .choice_answer import ChoiceAnswer
