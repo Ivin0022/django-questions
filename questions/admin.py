5cf05e227c11e030492389119f7440178dcda519
ivin stephen <ivinstephen001@gmail.com>
2020-05-25 02:35:08 +0530

    added text question model

--- questions/admin.py
+++ questions/admin.py
@@ -1,10 +1,16 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
 
-from .models import TextQuestion
+from .models import TextQuestion, TextAnswer
 
 
 @admin.register(TextQuestion)
 class TextQuestionAdmin(admin.ModelAdmin):
     list_display = ('id', 'title_text')
     list_display_links = ('title_text',)
+
+
+@admin.register(TextAnswer)
+class TextAnswerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'question', 'answer')
+    list_filter = ('user', 'question')
--- questions/models.py
+++ questions/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 
 class TextQuestion(models.Model):
@@ -15,3 +16,21 @@ class TextQuestion(models.Model):
     def __str__(self):
         """Unicode representation of TextQuestion."""
         return f'{self.title_text}'
+
+
+class TextAnswer(models.Model):
+    """Model definition for TextAnswer."""
+
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    question = models.ForeignKey('TextQuestion', on_delete=models.CASCADE)
+    answer = models.CharField('Answer', max_length=100)
+
+    class Meta:
+        """Meta definition for TextAnswer."""
+
+        verbose_name = 'Text Answer'
+        verbose_name_plural = 'Text Answers'
+
+    def __str__(self):
+        """Unicode representation of TextAnswer."""
+        return f'{self.answer}'
