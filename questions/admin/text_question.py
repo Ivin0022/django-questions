00bb034676244066734b33f105c2f48070c3e560
ivin stephen <ivinstephen001@gmail.com>
2020-05-25 02:47:48 +0530

    separated admin into files

--- questions/admin/text_question.py
+++ questions/admin/text_question.py
@@ -1,16 +1,10 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
 
-from .models import TextQuestion, TextAnswer
+from ..models import TextQuestion
 
 
 @admin.register(TextQuestion)
 class TextQuestionAdmin(admin.ModelAdmin):
     list_display = ('id', 'title_text')
     list_display_links = ('title_text',)
-
-
-@admin.register(TextAnswer)
-class TextAnswerAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user', 'question', 'answer')
-    list_filter = ('user', 'question')
